08de0765e8764890558277338bd9589a768b7402
abanoubfahmy <168681073+abanoubfahmy@users.noreply.github.com>
2024-05-18 21:16:32 +0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,8 @@
+let bigImg = document.querySelector(".big-img");
+  let container=document.querySelector(".container");
+
+        function mywatch(watch) {
+            bigImg.src = watch;}
+            function backGround(color){
+                container.style.background=color;}
+            
